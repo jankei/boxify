e974b60a899e8bdc3203edfbaaf4cdc5224ccfa7
Yaniv Keinan <yanivkeinan@gmail.com>
2016-02-22 22:51:27 +0200

    get all users

--- restApi/lib/users.js
+++ restApi/lib/users.js
@@ -1,11 +1,14 @@
 'use strict';
 
 module.exports = {
-  getUser : getUser
+  getUser : getUser,
+  read    : read
 };
 
+var async     = require('async');
 var db        = require('./db');
 var config    = require('./config');
+var util      = require('./util');
 
 function getUser(id, cb) {
   var params = {
@@ -15,3 +18,25 @@ function getUser(id, cb) {
   return db.get(params, cb);
 }
 
+function getAllUsers(cb) {
+  var params = {
+    TableName : config.tables.users,
+    ConsistentRead: true,
+    ReturnConsumedCapacity: "TOTAL"
+  };
+  return db.scan(params, cb);
+}
+
+function read(event, cb) {
+  async.waterfall([
+      function(waterfallCB) {
+        getAllUsers(waterfallCB);
+      },
+      function(result, waterfallCB) {
+        waterfallCB(null, util.pluckMany(result, 'id', 'name'));
+      }
+    ],
+    function(err, result) {
+      return cb(err, result);
+    });
+}
--- restApi/lib/util.js
+++ restApi/lib/util.js
@@ -2,10 +2,24 @@
 
 var logger = require('./logger');
 var uuid   = require('node-uuid');
+var _      = require('underscore');
 
 var util = {
-  uuid  : uuid.v4,
-  log   : logger.log
+  uuid      : uuid.v4,
+  log       : logger.log,
+  pluckMany : pluckMany
 };
 
 module.exports = util;
+
+function pluckMany() {
+  var source = arguments[0];
+  var propertiesToPluck = _.rest(arguments, 1);
+  return _.map(source, function(item) {
+    var obj = {};
+    _.each(propertiesToPluck, function(property) {
+      obj[property] = item[property];
+    });
+    return obj;
+  });
+}
